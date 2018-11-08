a3601397e4c888b963e76dc3e54ff485f87e31d6
James DeVore <jdevore4592@gmail.com>
2018-11-08 17:32:43 -0500

    Completed basic project

---
+++ main.js
@@ -0,0 +1,107 @@
+//Module to control the selection of perks
+var perkChooser = function() {
+  //Get four perk objects at random
+  var chooseRandomFourObjects = function(perkObjectArray) {
+    var randomPerkIndexes = [];
+    for (var i = 0; i < 4; i++) {
+      var randomSurvivorPerkIndex = Math.floor(Math.random() * perkObjectArray.length);
+      // I need to implement a no-repeat condition.
+      randomPerkIndexes.push(randomSurvivorPerkIndex);
+    }
+    if (checkForDuplicatePerks(randomPerkIndexes)) {
+      console.log("I found a duplicate!");
+      randomPerkIndexes = []
+      $('.results-display').html("Oops. There was a duplicate and I haven't figured out how to fix that. Please try again :)")
+    }
+    //I want to make a new array using the 4 chosen indexes
+    var randomPerkObjectsArray = [];
+    for (var i = 0; i < 4; i++) {
+      randomPerkObjectsArray.push(perkObjectArray[randomPerkIndexes[i]])
+    }
+    var randomPerkArrayInObject = {
+      perks: randomPerkObjectsArray
+    }
+    return randomPerkArrayInObject
+  };
+
+  var checkForDuplicatePerks = function(perkArray) {
+    var sortedPerkArray = perkArray.sort(function(a, b) {
+      return a - b
+    });
+    for (var i = 0; i < sortedPerkArray.length - 1; i++) {
+      if (sortedPerkArray[i] === sortedPerkArray[i + 1]) {
+        return true
+      }
+    }
+    return false;
+  };
+
+  var displayResults = function(dataObject) {
+    var source = $('#perk-display-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(dataObject);
+    $('.results-display').append(newHTML);
+  };
+
+
+//I am trying to filter the main array into an array without the specified properties
+var perkFilterFunction = function(allPerkArray,curtainCall,shatteredBloodline,halloween,jigsaw){
+  var filteredPerks = [];
+  curtainCall == 'true'? curtainCall = 'curtainCall' : curtainCall = false;
+  shatteredBloodline == 'true'? shatteredBloodline = "shatteredBloodline" : shatteredBloodline = false;
+  halloween == 'true'? halloween = "halloween" : halloween = false;
+  jigsaw == 'true'? jigsaw = 'jigsaw' : jigsaw = false;
+  filteredPerks = allPerkArray.filter(function(perkObjects){
+    //for each one, I want to ask: does it have a desired property
+
+    return (perkObjects.dlc == curtainCall || perkObjects.dlc == shatteredBloodline || perkObjects.dlc == halloween || perkObjects.dlc == jigsaw || perkObjects.dlc == 'baseGame')
+  })
+
+  return filteredPerks;
+}
+
+  return {
+    chooseRandomFourObjects:chooseRandomFourObjects,
+    displayResults:displayResults,
+    perkFilterFunction:perkFilterFunction
+  };
+
+}
+//I want to choose 4 perks at random from the master array, and populate the HTML with the information from the randomly chosen perks
+$(document).ready(function() {
+  $('button').on('click', function() {
+    $('.results-display').empty();
+    var $curtainCallDlc = $('#curtainCall').attr('value');
+    var $shatteredBloodlineDlc = $('#shatteredBloodline').attr('value');
+    var $halloweenDlc = $('#halloween').attr('value');
+    var $jigsawDlc = $('#jigsaw').attr('value');
+    var perks = perkChooser()
+    // var filteredSurvivorPerks = perks.perkFilterFunction(allSurvivorPerks,$curtainCallDlc,$shatteredBloodlineDlc,$halloweenDlc,$jigsawDlc);
+      var filteredSurvivorPerks = perks.perkFilterFunction(allSurvivorPerks,$curtainCallDlc,$shatteredBloodlineDlc,$halloweenDlc,$jigsawDlc);
+    var randomResultsObject = perks.chooseRandomFourObjects(filteredSurvivorPerks);
+    perks.displayResults(randomResultsObject)
+  });
+  $('.DLC').on('click', function() {
+  var $check = $(this).attr('value');
+  if ($check == 'true') {
+    $(this).attr('value', 'false')
+  } else {
+    $(this).attr('value', 'true');
+  }
+})
+});
+//
+// var perkFilterFunction = function(allPerkArray,curtainCall,shatteredBloodline,halloween,jigsaw){
+//   var filteredPerks = [];
+//   curtainCall == 'true'? curtainCall = 'curtainCall' : curtainCall = false;
+//   shatteredBloodline == 'true'? shatteredBloodline = "shatteredBloodline" : shatteredBloodline = false;
+//   halloween == 'true'? halloween = "halloween" : halloween = false;
+//   jigsaw == 'true'? jigsaw = 'jigsaw' : jigsaw = false;
+//   filteredPerks = allPerkArray.filter(function(perkObjects){
+//     //for each one, I want to ask: does it have a desired property
+//
+//     return (perkObjects.dlc == curtainCall || perkObjects.dlc == shatteredBloodline || perkObjects.dlc == halloween || perkObjects.dlc == jigsaw || perkObjects.dlc == 'baseGame')
+//   })
+//
+//   return filteredPerks;
+// }
---
+++ perks.js
@@ -0,0 +1,308 @@
+var allSurvivorPerks =[
+    {
+      name: 'ace In The Hole',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/2/24/IconPerks_aceInTheHole.png/150px-IconPerks_aceInTheHole.png?version=82043e98b2a502b029ec1ed321d650bf',
+      dlc: 'baseGame',
+      survivor: 'Ace'
+    },
+    {
+      name: 'adrenaline',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/f7/IconPerks_adrenaline.png/150px-IconPerks_adrenaline.png?version=010b7bf4397baac9c12c740ba95a1ee8',
+      dlc: 'baseGame',
+      survivor: 'Meg'
+    },
+    {
+      name: 'alert',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/7/71/IconPerks_alert.png/150px-IconPerks_alert.png?version=a50dceb1e39e381389228a9474743d10',
+      dlc: 'baseGame',
+      survivor: 'Feng'
+    },
+    {
+      name: 'autodidact',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/9/96/IconPerks_autodidact.png/150px-IconPerks_autodidact.png?version=730e5c7691050673e0d6355667cc7e57',
+      dlc: 'shatteredBloodline',
+      survivor: 'Adam'
+    },
+    {
+      name: 'balanced Landing',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/5/5e/IconPerks_balancedLanding.png/150px-IconPerks_balancedLanding.png?version=97b013f33b363dca4f2e3073d82dba9f',
+      dlc: 'baseGame',
+      survivor: 'Nea'
+    },
+    {
+      name: 'boil Over',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/f5/IconPerks_boilOver.png/150px-IconPerks_boilOver.png?version=8ef755c35968a9f60d14e45d9607a49d',
+      dlc: 'curtainCall',
+      survivor: 'Kate'
+    },
+    {
+      name: 'bond',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/a/a0/IconPerks_bond.png/150px-IconPerks_bond.png?version=8e41f54dfad8c4e68cb2140c6442cba1',
+      dlc: 'baseGame',
+      survivor: 'Dwight'
+    },
+    {
+      name: 'borrowed Time',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/a/a3/IconPerks_borrowedTime.png/150px-IconPerks_borrowedTime.png?version=4b03beb42b4474b286e1116c9ea34e17',
+      dlc: 'baseGame',
+      survivor: 'Bill'
+    },
+    {
+      name: 'botany Knowledge',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/1/1e/IconPerks_botanyKnowledge.png/150px-IconPerks_botanyKnowledge.png?version=7cb6a56490b31b832eed7ba7b958cd1b',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'calm Spirit',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/6/6c/IconPerks_calmSpirit.png/150px-IconPerks_calmSpirit.png?version=ccbc280dbb70495b1b7e9bda089e4e81',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'dance With Me',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/1/17/IconPerks_danceWithMe.png/150px-IconPerks_danceWithMe.png?version=ffda76ee12c53f6b1fa9deba1043f536',
+      dlc: 'curtainCall',
+      survivor: 'Kate'
+    },
+    {
+      name: 'dark Sense',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/9/92/IconPerks_darkSense.png/150px-IconPerks_darkSense.png?version=c5d2575a6dde57f4d9b048767285c1d3',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'dead Hard',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/7/75/IconPerks_deadHard.png/150px-IconPerks_deadHard.png?version=893249f51b4d262c81010fa284937d3e',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'decisive Strike',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/e/ee/IconPerks_decisiveStrike.png/150px-IconPerks_decisiveStrike.png?version=e3dc74f65cc96316fcdd2d4b2560342d',
+      dlc: 'halloween'
+    },
+    {
+      name: 'deja Vu',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/8/89/IconPerks_dejaVu.png/150px-IconPerks_dejaVu.png?version=11afafe0fb3665359bbea553e9b77696',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'deliverance',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/7/7e/IconPerks_deliverance.png/150px-IconPerks_deliverance.png?version=a95dc8c21120568eff452713620856fe',
+      dlc: 'shatteredBloodline',
+      survivor: 'Adam'
+    },
+    {
+      name: 'detectives Hunch',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/7/7b/IconPerks_detectivesHunch.png/150px-IconPerks_detectivesHunch.png?version=f07618d27910ce15720270c635e97a76',
+      dlc: 'jigsaw'
+    },
+    {
+      name: 'diversion',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/a/a6/IconPerks_diversion.png/150px-IconPerks_diversion.png?version=1203757cff49ecdda2804e257d0cbdad',
+      dlc: 'shatteredBloodline',
+      survivor: 'Adam'
+    },
+    {
+      name: 'empathy',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/a/ae/IconPerks_empathy.png/150px-IconPerks_empathy.png?version=416378e866746bc453f2ecbd0421c661',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'hope',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/1/18/IconPerks_hope.png/150px-IconPerks_hope.png?version=17381bb8c8c6b84852263efd9a873e26',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'iron Will',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/5/58/IconPerks_ironWill.png/150px-IconPerks_ironWill.png?version=8da8426661c3b4aa37093be24f02100b',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'kindred',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/a/a8/IconPerks_kindred.png/150px-IconPerks_kindred.png?version=060dc43341886d8cde50e499fdc14822',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'leader',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/3/3d/IconPerks_leader.png/150px-IconPerks_leader.png?version=fc5a1e3d3e38abd5c8f3f483706a95a7',
+      dlc: 'baseGame',
+      survivor: 'Dwight'
+    },
+    {
+      name: 'left Behind',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/c/c3/IconPerks_leftBehind.png/150px-IconPerks_leftBehind.png?version=6aaf4af174733f95748ed2c601edeedd',
+      dlc: 'baseGame',
+      survivor: 'Bill'
+    },
+    {
+      name: 'lightweight',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/6/67/IconPerks_lightweight.png/150px-IconPerks_lightweight.png?version=3c0bc9f9b53fb82c88738e2ba9d417ba',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'lithe',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/c/cc/IconPerks_lithe.png/150px-IconPerks_lithe.png?version=0c8fba2076aceec8b750a0139ad7d660',
+      dlc: 'baseGame',
+      survivor: 'Feng'
+    },
+    {
+      name: 'no Mither',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/f5/IconPerks_noMither.png/150px-IconPerks_noMither.png?version=21aeb52d3a90b270426b824f47851729',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'no One Left Behind',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/b/b0/IconPerks_noOneLeftBehind.png/150px-IconPerks_noOneLeftBehind.png?version=e3092e6935ea1568a56b9e7a5367d8eb',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'object Of Obsession',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/2/2b/IconPerks_objectOfObsession.png/150px-IconPerks_objectOfObsession.png?version=e5f9235c51d63cab31c172633e3df354',
+      dlc: 'halloween'
+    },
+    {
+      name: 'open Handed',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/5/5d/IconPerks_open-Handed.png/150px-IconPerks_open-Handed.png?version=46f8bea772b000c9355062215072fd97',
+      dlc: 'baseGame',
+      survivor: 'Ace'
+    },
+    {
+      name: 'pharmacy',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/f1/IconPerks_pharmacy.png/150px-IconPerks_pharmacy.png?version=bec1b6a5ea7530706c8a536bffc49317',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'plunderers Instinct',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/2/28/IconPerks_plunderersInstinct.png/150px-IconPerks_plunderersInstinct.png?version=4adeb1bb9206de0941553dcba29ef82f',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'premonition',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/3/39/IconPerks_premonition.png/150px-IconPerks_premonition.png?version=53c4a7ab87e44c0bea91ba2d2e725f75',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'prove Thyself',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/0/0f/IconPerks_proveThyself.png/150px-IconPerks_proveThyself.png?version=6576a48f7ae7a8544fc5deb8d3da4b57',
+      dlc: 'baseGame',
+      survivor: 'Dwight'
+    },
+    {
+      name: 'quick And Quiet',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/1/14/IconPerks_quickAndQuiet.png/150px-IconPerks_quickAndQuiet.png?version=c0b257b4765923881326312d618c5361',
+      dlc: 'baseGame',
+      survivor: 'Meg'
+    },
+    {
+      name: 'resilience',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/e/ee/IconPerks_resilience.png/150px-IconPerks_resilience.png?version=9a48eb561a7da43b585e73a044dd8e97',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'saboteur',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/0/0e/IconPerks_saboteur.png/150px-IconPerks_saboteur.png?version=0386307573739875da4d770195f79db2',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'self Care',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/f1/IconPerks_selfCare.png/150px-IconPerks_selfCare.png?version=e301adb1f5720cfee08e35ca2b9e489e',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'slippery Meat',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/0/03/IconPerks_slipperyMeat.png/150px-IconPerks_slipperyMeat.png?version=edf7a3d4e24583d261a925f0a69ccdef',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'small Game',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/d/dc/IconPerks_smallGame.png/150px-IconPerks_smallGame.png?version=775fa0256342d42263ef91efb5bc77a1',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'sole Survivor',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/8/8e/IconPerks_soleSurvivor.png/150px-IconPerks_soleSurvivor.png?version=4818014ea059c0f08d9b38dc1266b2f8',
+      dlc: 'halloween'
+    },
+    {
+      name: 'spine Chill',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/5/57/IconPerks_spineChill.png/150px-IconPerks_spineChill.png?version=028a4534269cb7bc0e8d349418b08e77',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'sprint Burst',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/e/e4/IconPerks_sprintBurst.png/150px-IconPerks_sprintBurst.png?version=cf464a31905de8c9bb1e4c2679e60153',
+      dlc: 'baseGame',
+      survivor: 'Meg'
+    },
+    {
+      name: 'stake Out',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/c/cd/IconPerks_stakeOut.png/150px-IconPerks_stakeOut.png?version=65a79c15ad3f00f8117ad33853827f80',
+      dlc: 'jigsaw'
+    },
+    {
+      name: 'streetwise',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/ff/IconPerks_streetwise.png/150px-IconPerks_streetwise.png?version=967dd9bebee03132251c6eb9c85fff11',
+      dlc: 'baseGame',
+      survivor: 'Nea'
+    },
+    {
+      name: 'this Is Not Happening',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/5/58/IconPerks_thisIsNotHappening.png/150px-IconPerks_thisIsNotHappening.png?version=f68a941ba6fc63bd6a817079c59f2d3b',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'technician',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/6/62/IconPerks_technician.png/150px-IconPerks_technician.png?version=b8ec04ede589f43fcaafd490581d78cd',
+      dlc: 'baseGame',
+      survivor: 'Feng'
+    },
+    {
+      name: 'tenacity',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/9/9d/IconPerks_tenacity.png/150px-IconPerks_tenacity.png?version=a76e1e21eead318bbb198bec68efb254',
+      dlc: 'jigsaw'
+    },
+    {
+      name: 'up The Ante',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/a/a3/IconPerks_upTheAnte.png/150px-IconPerks_upTheAnte.png?version=54a4a95ae7efb8217a2fb66bfba90f32',
+      dlc: 'baseGame',
+      survivor: 'Ace'
+    },
+    {
+      name: 'unbreakable',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/4/4a/IconPerks_unbreakable.png/150px-IconPerks_unbreakable.png?version=82cb3d2c1a78ec983e8f61c5cfcd2d06',
+      dlc: 'baseGame',
+      survivor: 'Bill'
+    },
+    {
+      name: 'urban Evasion',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/c/c7/IconPerks_urbanEvasion.png/150px-IconPerks_urbanEvasion.png?version=2c4419474dc016feebfac51cc2227422',
+      dlc: 'baseGame',
+      survivor: 'Nea'
+    },
+    {
+      name: 'vigil',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/4/46/IconPerks_vigil.png/150px-IconPerks_vigil.png?version=6dcee279a103a1b6fa62c481a50fee9b',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'wake Up',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/f/fd/IconPerks_wakeUp.png/150px-IconPerks_wakeUp.png?version=ee73c7fa63a1ebaa73106a3527086aa5',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'well Make It',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/8/8a/IconPerks_wellMakeIt.png/150px-IconPerks_wellMakeIt.png?version=3e216c9e3135dcada388f7fa11b4091c',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'we\'re Gonna Live Forever',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/b/b8/IconPerks_wereGonnaLiveForever.png/150px-IconPerks_wereGonnaLiveForever.png?version=dee4b0d8d690499715bfe6cc943d2552',
+      dlc: 'baseGame'
+    },
+    {
+      name: 'windows Of Opporunity',
+      url: 'https://d1u5p3l4wpay3k.cloudfront.net/deadbydaylight_gamepedia_en/thumb/1/17/IconPerks_windowsOfOpportunity.png/150px-IconPerks_windowsOfOpportunity.png?version=0943796fdd385007ca758d45ca80567c',
+      dlc: 'curtainCall',
+      survivor: 'Kate'
+    }
+  ]
+
+var allKillerPerks = []
